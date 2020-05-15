563965a30e55fe96d328b34c4b32f20ce1181314
Steven Roose <steven@stevenroose.org>
2020-05-15 11:06:36 +0100

    Update bitcoin_hashes dependency and remove hex dep

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 extern crate bitcoin_hashes;
-#[cfg(test)]
-extern crate hex;
 #[cfg(feature = "serde")]
 extern crate serde;
 
@@ -59,15 +57,15 @@ impl hash::Hash for Node {
 }
 
 impl ::std::str::FromStr for Node {
-	type Err = ::bitcoin_hashes::Error;
+	type Err = ::bitcoin_hashes::hex::Error;
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
 		::bitcoin_hashes::hex::FromHex::from_hex(s)
 	}
 }
 
 impl ::bitcoin_hashes::hex::FromHex for Node {
-	fn from_byte_iter<I>(iter: I) -> Result<Self, ::bitcoin_hashes::Error>
-		where I: Iterator<Item=Result<u8, ::bitcoin_hashes::Error>> +
+	fn from_byte_iter<I>(iter: I) -> Result<Self, ::bitcoin_hashes::hex::Error>
+		where I: Iterator<Item=Result<u8, ::bitcoin_hashes::hex::Error>> +
 			ExactSizeIterator +
 			DoubleEndedIterator,
 	{
@@ -236,19 +234,25 @@ impl Node {
 mod tests {
 	use super::*;
 
-	use hex;
+	use bitcoin_hashes::hex::{FromHex, ToHex};
 
 	#[test]
 	fn slip_example() {
-		let seed = hex::decode("c76c4ac4f4e4a00d6b274d5c39c700bb4a7ddc04fbc6f78e85ca75007b5b495f74a9043eeb77bdd53aa6fc3a0e31462270316fa04b8c19114c8798706cd02ac8").unwrap();
+		let seed = Vec::<u8>::from_hex(
+			"c76c4ac4f4e4a00d6b274d5c39c700bb4a7ddc04fbc6f78e85ca75007b5b495f\
+			 74a9043eeb77bdd53aa6fc3a0e31462270316fa04b8c19114c8798706cd02ac8"
+		).unwrap();
 
 		let master = Node::new_master(&seed);
-		assert_eq!(master.key(), &hex::decode("dbf12b44133eaab506a740f6565cc117228cbf1dd70635cfa8ddfdc9af734756").unwrap()[..]);
+		assert_eq!(master.key().to_hex(),
+			"dbf12b44133eaab506a740f6565cc117228cbf1dd70635cfa8ddfdc9af734756");
 
 		let child1 = master.derive_child("SLIP-0021".as_bytes());
-		assert_eq!(child1.key(), &hex::decode("1d065e3ac1bbe5c7fad32cf2305f7d709dc070d672044a19e610c77cdf33de0d").unwrap()[..]);
+		assert_eq!(child1.key().to_hex(),
+			"1d065e3ac1bbe5c7fad32cf2305f7d709dc070d672044a19e610c77cdf33de0d");
 
 		let child2 = child1.derive_child("Master encryption key".as_bytes());
-		assert_eq!(child2.key(), &hex::decode("ea163130e35bbafdf5ddee97a17b39cef2be4b4f390180d65b54cf05c6a82fde").unwrap()[..]);
+		assert_eq!(child2.key().to_hex(),
+			"ea163130e35bbafdf5ddee97a17b39cef2be4b4f390180d65b54cf05c6a82fde");
 	}
 }
